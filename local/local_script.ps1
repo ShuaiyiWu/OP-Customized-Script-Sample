7a54f26aabd5c957b54e77f8cbd7ca6b5ba4c0c9
Shuaiyi Wu <ShuaiyiWu@users.noreply.github.com>
2016-07-18 12:00:23 +0800

    Update local_script.ps1

--- local/local_script.ps1
+++ local/local_script.ps1
@@ -5,4 +5,8 @@ param (
 
 Write-Host "Local script test succeeded."
 
+LogInfo -logFilePath $reportFilePath -message "Log Info test" -source $MyInvocation.MyCommand.Definition 
+LogVerbose -logFilePath $reportFilePath -message "Log Verbose test" -source $MyInvocation.MyCommand.Definition 
+
+
 exit $LASTEXITCODE
